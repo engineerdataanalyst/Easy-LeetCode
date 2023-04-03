2411b0dab4c3469694f67618cab2894f6da621e0
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-03 05:38:32 -0400

    Update and rename Swap Salary to Swap Salary.sql

--- Swap Salary.sql
+++ Swap Salary.sql
@@ -4,6 +4,6 @@
 
 UPDATE Salary
 SET sex = CASE
-            WHEN sex = 'm' THEN 'f'
-            WHEN sex = 'f' THEN 'm'
+              WHEN sex = 'm' THEN 'f'
+              WHEN sex = 'f' THEN 'm'
           END;
