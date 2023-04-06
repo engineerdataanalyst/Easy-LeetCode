228593f80728123e318a61c5b8246f50241bd516
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-06 15:13:37 -0400

    Create Combine Two Tables.sql

---
+++ 1.) Easy/Combine Two Tables.sql
@@ -0,0 +1,12 @@
+/* Write an SQL query to report the first name, last name, city,
+   and state of each person in the Person table.
+   If the address of a personId is not present
+   in the Address table, report null instead. */
+
+SELECT
+    P.firstName,
+    P.lastName,
+    A.city,
+    A.state
+FROM Person P
+LEFT JOIN Address A ON P.personId = A.personId;
