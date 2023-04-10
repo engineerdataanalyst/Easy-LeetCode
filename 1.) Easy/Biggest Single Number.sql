93543cea1b1a50715969eea83255e5e432c22a2a
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-10 17:01:35 -0400

    Create Biggest Single Number.sql

---
+++ 1.) Easy/Biggest Single Number.sql
@@ -0,0 +1,13 @@
+/* A single number is a number that appeared only once in the MyNumbers table.
+   Write an SQL query to report the largest single number. 
+   If there is no single number, report null. */
+   
+SELECT MAX(num) AS num
+FROM
+    (SELECT 
+        CASE 
+            WHEN num/SUM(num) != 1 THEN NULL
+            ELSE num
+        END AS num
+     FROM MyNumbers
+     GROUP BY num) AS one;
