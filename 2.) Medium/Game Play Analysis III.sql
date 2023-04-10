d11a26db94d73fd139d806c91316d75e8e6c7415
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-10 19:57:07 -0400

    Create Game Play Analysis III.sql

---
+++ 2.) Medium/Game Play Analysis III.sql
@@ -0,0 +1,8 @@
+/* Write an SQL query to report for each player and date, how many games played so far by the player. 
+   That is, the total number of games played by the player until that date. */
+
+SELECT
+    player_id,
+    event_date,
+    SUM(games_played) OVER(PARTITION BY player_id ORDER BY event_date) AS games_played_so_far
+FROM Activity;
