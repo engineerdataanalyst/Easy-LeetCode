1c0559a13b21800784286236f75b261b05b3f389
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-10 17:10:28 -0400

    Create Sales Analysis I.sql

---
+++ 1.) Easy/Sales Analysis I.sql
@@ -0,0 +1,13 @@
+/* Write an SQL query that reports 
+   the best seller by total sales price, 
+   If there is a tie, report them all. */
+
+SELECT seller_id
+FROM Sales
+GROUP BY seller_id
+HAVING SUM(price) =
+    (SELECT MAX(price_sum)
+     FROM
+        (SELECT SUM(price) AS price_sum
+         FROM Sales
+         GROUP BY seller_id) t)
