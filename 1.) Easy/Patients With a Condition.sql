3f7025b29bca3ca07c2fb5cc170006cfbc137a76
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-10 17:37:19 -0400

    Create Patients With a Condition.sql

---
+++ 1.) Easy/Patients With a Condition.sql
@@ -0,0 +1,12 @@
+/* Write an SQL query to report 
+   the patient_id, patient_name and conditions of 
+   the patients who have Type I Diabetes. 
+   Type I Diabetes always starts with DIAB1 prefix. */
+
+SELECT
+    patient_id,
+    patient_name,
+    conditions
+FROM Patients
+WHERE conditions LIKE '% DIAB1%' OR
+      conditions LIKE 'DIAB1%';
