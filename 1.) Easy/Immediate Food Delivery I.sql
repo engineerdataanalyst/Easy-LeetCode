f011a265a6cb0c1bcc5779b5266a5b8e348e25a5
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-10 17:16:48 -0400

    Create Immediate Food Delivery I.sql

---
+++ 1.) Easy/Immediate Food Delivery I.sql
@@ -0,0 +1,13 @@
+/* If the customer's preferred delivery date is the same as the order date, 
+   then the order is called immediate; otherwise, it is called scheduled.
+
+   Write an SQL query to find 
+   the percentage of immediate orders in the table, 
+   rounded to 2 decimal places. */
+
+SELECT
+    ROUND(COUNT(CASE
+                    WHEN order_date = customer_pref_delivery_date
+                    THEN customer_pref_delivery_date
+                ELSE NULL END)/COUNT(order_date)*100, 2) AS immediate_percentage
+FROM Delivery;
