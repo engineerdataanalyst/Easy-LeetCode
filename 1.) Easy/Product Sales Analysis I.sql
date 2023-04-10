5e76e30b934b004d8ceac7691507abb9a71211d6
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-10 17:06:20 -0400

    Create Product Sales Analysis I.sql

---
+++ 1.) Easy/Product Sales Analysis I.sql
@@ -0,0 +1,10 @@
+/* Write an SQL query that reports
+   the product_name, year, and price for each sale_id
+   in the Sales table. */
+ 
+SELECT
+    P.product_name,
+    S.year,
+    S.price
+FROM Product P
+INNER JOIN Sales S ON P.product_id = S.product_id;
