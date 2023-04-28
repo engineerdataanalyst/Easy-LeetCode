d0a54f8317e88472c0009c1e0da96f6b4c03bd38
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-28 17:02:50 -0400

    Create List the Products Ordered in a Period.sql

---
+++ 1.) Easy/List the Products Ordered in a Period.sql
@@ -0,0 +1,8 @@
+SELECT
+    P.product_name,
+    SUM(O.unit) AS unit
+FROM Products P
+LEFT JOIN Orders O ON P.product_id = O.product_id
+WHERE MONTH(O.order_date) = 2 AND YEAR(O.order_date) = 2020
+GROUP BY P.product_id
+HAVING unit >= 100;
