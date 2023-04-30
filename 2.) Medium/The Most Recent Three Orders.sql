39cdaf3316ea3947be86ce13ec89743af9198569
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-30 10:21:55 -0400

    Update The Most Recent Three Orders.sql

--- 2.) Medium/The Most Recent Three Orders.sql
+++ 2.) Medium/The Most Recent Three Orders.sql
@@ -1,3 +1,10 @@
+/* Write an SQL query to find the most recent three orders of each user. 
+   If a user ordered less than three orders, return all of their orders.
+
+   Return the result table ordered by customer_name in ascending order 
+   and in case of a tie by the customer_id in ascending order. 
+   If there is still a tie, order them by order_date in descending order. */
+
 WITH T AS
 (
     SELECT
