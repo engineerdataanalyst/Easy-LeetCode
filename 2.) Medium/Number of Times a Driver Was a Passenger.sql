2fcf8658ad6bb0b2ad385a473326c66053000b24
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-05-23 19:54:44 -0400

    Create Number of Times a Driver Was a Passenger.sql

---
+++ 2.) Medium/Number of Times a Driver Was a Passenger.sql
@@ -0,0 +1,12 @@
+-- Write an SQL query to report the ID of each driver and the number of times they were a passenger.
+
+SELECT
+    driver_id,
+    (
+        SELECT COUNT(*)
+        FROM Rides R1
+        WHERE R2.driver_id = R1.passenger_id
+    ) AS cnt
+FROM Rides R2
+GROUP BY driver_id
+ORDER BY driver_id;
