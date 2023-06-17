8a98f2af9f9a25b5e406e84d5b9f869e5e3fdca6
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-06-17 09:17:31 -0400

    Create The Airport With the Most Traffic.sql

---
+++ 2.) Medium/The Airport With the Most Traffic.sql
@@ -0,0 +1,34 @@
+/* Write an SQL query to report the ID of the airport with the most traffic.
+   The airport with the most traffic is the airport that has the largest total number of flights that either departed from or arrived at the airport. 
+   If there is more than one airport with the most traffic, report them all. */
+
+WITH Airports AS
+(
+    SELECT
+        departure_airport AS airport_id,
+        flights_count
+    FROM Flights
+
+    UNION ALL
+
+    SELECT
+        arrival_airport AS airport_id,
+        flights_count
+    FROM Flights
+),
+FlightRanks AS
+(
+    SELECT
+        *,
+        DENSE_RANK() OVER(ORDER BY total_flights DESC) AS rank_num
+    FROM
+    (
+        SELECT
+            airport_id,
+            SUM(flights_count) AS total_flights
+        FROM Airports
+        GROUP BY airport_id
+    ) A
+)
+SELECT airport_id
+FROM FlightRanks
