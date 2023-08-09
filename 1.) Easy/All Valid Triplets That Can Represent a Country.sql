f93d31aaf82889be248a9a9387d409d22d4e60a5
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-08-09 10:30:08 -0400

    Update All Valid Triplets That Can Represent a Country.sql

--- 1.) Easy/All Valid Triplets That Can Represent a Country.sql
+++ 1.) Easy/All Valid Triplets That Can Represent a Country.sql
@@ -1,3 +1,13 @@
+/* There is a country with three schools, where each student is enrolled in exactly one school.
+   The country is joining a competition and wants to select one student from each school to represent the country such that:
+
+   member_A is selected from SchoolA,
+   member_B is selected from SchoolB,
+   member_C is selected from SchoolC, and
+   The selected students' names and IDs are pairwise distinct (i.e. no two students share the same name, and no two students share the same ID).
+
+   Write an SQL query to find all the possible triplets representing the country under the given constraints. */
+
 SELECT
     A.student_name AS member_A,
     B.student_name AS member_B,
