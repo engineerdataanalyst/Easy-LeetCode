bc649ea8a96f0b685356b649776fd9106056eaad
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-08-11 11:28:44 -0400

    Update The Number of Passengers in Each Bus II.sql

--- 3.) Hard/The Number of Passengers in Each Bus II.sql
+++ 3.) Hard/The Number of Passengers in Each Bus II.sql
@@ -1,3 +1,14 @@
+/* Buses and passengers arrive at the LeetCode station.
+    
+   If a bus arrives at the station at a time tbus and a passenger arrived at a time tpassenger where tpassenger <= tbus =
+   and the passenger did not catch any bus, the passenger will use that bus. In addition, each bus has a capacity.
+
+   If at the moment the bus arrives at the station there are more passengers waiting than its capacity capacity, only capacity passengers will use the bus.
+
+   Write an SQL query to report the number of users that used each bus.
+
+   Return the result table ordered by bus_id in ascending order. */
+
 WITH RECURSIVE NewBuses AS
 (
     SELECT
