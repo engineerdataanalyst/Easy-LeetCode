3a45908f925de7835be0ac67f6b091767b45eb6a
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-10-22 03:32:16 -0400

    Update pain.sql

--- Jess Ramos/Solve Real-World Data Problems with SQL/pain.sql
+++ Jess Ramos/Solve Real-World Data Problems with SQL/pain.sql
@@ -1,1 +1,21 @@
-
+WITH MonthlyRevenue AS
+(
+    SELECT
+        P.ProductName,
+        MONTH(S.OrderDate) AS OrderMonth,
+        SUM(S.Revenue) AS TotalRevenue
+    FROM Subscriptions S
+    LEFT JOIN Products P ON S.ProductID = P.ProductId
+    WHERE YEAR(S.OrderDate) = 2022
+    GROUP BY
+        P.ProductName,
+        OrderMonth
+)
+SELECT
+    ProductName,
+    MIN(TotalRevenue) AS MIN_REV,
+    MAX(TotalRevenue) AS MAX_REV,
+    AVG(TotalRevenue) AS AVG_REV,
+    STDDEV(TotalRevenue) AS STD_DEV_REV
+FROM MonthlyRevenue
+GROUP BY ProductName
