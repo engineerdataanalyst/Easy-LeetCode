19bd9b3e99b16af8d4595b5ac64de612b7052062
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-10-22 03:46:13 -0400

    Create 6.) Combine product tables with UNION.sql

---
+++ Jess Ramos/Solve Real-World Data Problems with SQL/6.) Combine product tables with UNION.sql
@@ -0,0 +1,17 @@
+WITH all_subscriptions AS
+(
+	  SELECT expirationdate
+	  FROM SubscriptionsProduct1
+	  WHERE active = 1
+
+	  UNION
+
+	  SELECT expirationdate
+	  FROM SubscriptionsProduct2
+	  WHERE active = 1
+)
+SELECT
+	  date_trunc('year', expirationdate) AS exp_year, 
+	  COUNT(*) AS subscriptions
+FROM all_subscriptions
+GROUP BY date_trunc('year', expirationdate);
