c9faeadcc2956ad7cb6738771363d796250337ec
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-10-22 03:40:21 -0400

    Create 3.) Payment funnel analysis with CTEs.sql

---
+++ Jess Ramos/Solve Real-World Data Problems with SQL/3.) Payment funnel analysis with CTEs.sql
@@ -0,0 +1,23 @@
+WITH MaxStatus AS
+(
+    SELECT
+		  SubscriptionID,
+		  MAX(StatusID) AS maxstatus
+	  FROM PaymentStatusLog
+	  GROUP BY SubscriptionID
+)
+SELECT
+	  CASE 
+		  WHEN M.maxstatus = 1 THEN 'PaymentWidgetOpened'
+		  WHEN M.maxstatus = 2 THEN 'PaymentEntered'
+		  WHEN M.maxstatus = 3 AND currentstatus = 0 THEN 'User Error with Payment Submission'
+		  WHEN M.maxstatus = 3 AND currentstatus != 0 THEN 'Payment Submitted'
+		  WHEN M.maxstatus = 4 AND currentstatus = 0 THEN 'Payment Processing Error with Vendor'
+		  WHEN M.maxstatus = 4 AND currentstatus != 0 THEN 'Payment Success'
+		  WHEN M.maxstatus = 5 THEN 'Complete'
+		  WHEN M.maxstatus IS NULL THEN 'User did not start payment process'
+	  END AS paymentfunnelstage,
+	  COUNT(*) AS subscriptions
+FROM Subscriptions S
+LEFT JOIN MaxStatus M ON M.SubscriptionID = S.subscriptionID
+GROUP BY paymentfunnelstage;
