3994277766e4c2e494ae928944b1f0140101f542
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-10-26 10:36:18 -0400

    Create The Number of Passengers in Each Bus I.sql

---
+++ LeetCode/2.) Medium/The Number of Passengers in Each Bus I.sql
@@ -0,0 +1,36 @@
+/* Buses and passengers arrive at the LeetCode station.
+
+   If a bus arrives at the station at time tbus and a passenger arrived at time tpassenger where tpassenger <= tbus 
+   and the passenger did not catch any bus, the passenger will use that bus.
+
+   Write a solution to report the number of users that used each bus.
+   Return the result table ordered by bus_id in ascending order. */
+
+WITH BusIds AS
+(
+    SELECT bus_id
+    FROM Buses
+),
+NewBuses AS
+(
+    SELECT *
+    FROM
+    (
+        SELECT
+            B.bus_id,
+            P.passenger_id,
+            B.arrival_time AS bus_arrival_time,
+            P.arrival_time AS passenger_arrival_time,
+            ROW_NUMBER() OVER(PARTITION BY P.passenger_id ORDER BY P.passenger_id, B.arrival_time, P.arrival_time) AS row_num
+        FROM Buses B
+        CROSS JOIN Passengers P ON B.arrival_time >= P.arrival_time
+    ) N
+    WHERE row_num = 1
+)
+SELECT
+    B.bus_id,
+    COUNT(N.passenger_id) AS passengers_cnt
+FROM Buses B
+LEFT JOIN NewBuses N ON B.bus_id = N.bus_id
+GROUP BY B.bus_id
+ORDER BY B.bus_id;
