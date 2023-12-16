5cb5ce48ad7a1e6cbb5fa845068423827208eee4
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-12-16 09:36:00 -0500

    Update Sales Person.sql

--- 1.) LeetCode/1.) Easy/Sales Person.sql
+++ 1.) LeetCode/1.) Easy/Sales Person.sql
@@ -1,10 +1,17 @@
 /* Write an SQL query to report the names of all the salespersons
    who did not have any orders related to the company with the name "RED". */
    
+WITH ComList AS
+(
+    SELECT
+        S.sales_id,
+        S.name,
+        COALESCE(CONCAT(' ', GROUP_CONCAT(DISTINCT C.name SEPARATOR ' '), ' '), 'N/A') AS com_list
+    FROM SalesPerson S
+    LEFT JOIN Orders O ON S.sales_id = O.sales_id
+    LEFT JOIN Company C ON O.com_id = C.com_id
+    GROUP BY S.sales_id
+)
 SELECT name
-FROM SalesPerson
-WHERE sales_id NOT IN
-    (SELECT sales_id
-     FROM Orders O
-     LEFT JOIN Company C ON O.com_id = C.com_id
-     WHERE C.name = 'RED');
+FROM ComList
+WHERE REGEXP_INSTR(com_list, ' RED ') = 0;
