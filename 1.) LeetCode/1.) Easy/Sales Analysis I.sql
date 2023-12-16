467b76285272aa8497df674a509140ac139627b9
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-12-16 08:53:09 -0500

    Update Sales Analysis I.sql

--- 1.) LeetCode/1.) Easy/Sales Analysis I.sql
+++ 1.) LeetCode/1.) Easy/Sales Analysis I.sql
@@ -2,12 +2,16 @@
    the best seller by total sales price, 
    If there is a tie, report them all. */
 
+WITH TotalSalesPrices AS
+(
+    SELECT
+        seller_id,
+        SUM(price) AS total_sales_prices,
+        DENSE_RANK() OVER(ORDER BY SUM(price) DESC) AS rank_num
+    FROM Sales
+    GROUP BY seller_id
+)
 SELECT seller_id
-FROM Sales
-GROUP BY seller_id
-HAVING SUM(price) =
-    (SELECT MAX(price_sum)
-     FROM
-        (SELECT SUM(price) AS price_sum
-         FROM Sales
-         GROUP BY seller_id) t)
+FROM TotalSalesPrices
+WHERE rank_num = 1
+ORDER BY seller_id;
