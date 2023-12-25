4412174d5a5eb227790b9cf5d46d6d2d0ec510b5
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-12-25 09:53:06 -0500

    Update Department Highest Salary.sql

--- 1.) LeetCode/2.) Medium/Department Highest Salary.sql
+++ 1.) LeetCode/2.) Medium/Department Highest Salary.sql
@@ -1,14 +1,18 @@
 -- Write an SQL query to find employees who have the highest salary in each of the departments.
 
+WITH RankNumbers AS
+(
+    SELECT
+        D.name AS Department,
+        E.name AS Employee,
+        E.salary AS Salary,
+        DENSE_RANK() OVER(PARTITION BY D.id ORDER BY E.salary DESC) AS rank_num
+    FROM Employee E
+    LEFT JOIN Department D ON E.departmentId = D.id
+)
 SELECT
-    D.name AS Department,
-    E.name AS Employee,
-    E.salary AS Salary
-FROM Employee E
-LEFT JOIN Department D ON E.departmentId = D.id
-WHERE (E.departmentId, E.salary) IN
-    (SELECT
-         departmentId,
-         MAX(salary)
-     FROM Employee
-     GROUP BY departmentId);
+    Department,
+    Employee,
+    Salary
+FROM RankNumbers
+WHERE rank_num = 1;
