2c9ac8eae873faa778c4cbd9d915648cf96bba8a
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-12-26 16:40:50 -0500

    Update Second Highest Salary.sql

--- 1.) LeetCode/2.) Medium/Second Highest Salary.sql
+++ 1.) LeetCode/2.) Medium/Second Highest Salary.sql
@@ -1,8 +1,21 @@
 /* Write an SQL query to report the second highest salary from the Employee table.
    If there is no second highest salary, the query should report null. */
 
-SELECT MAX(salary) AS SecondHighestSalary
-FROM Employee
-WHERE salary <
-    (SELECT MAX(salary)
-     FROM Employee)
+WITH NewEmployee AS
+(
+    SELECT
+        id,
+        salary,
+        DENSE_RANK() OVER(ORDER BY salary DESC) AS rank_num
+    FROM Employee
+)
+SELECT DISTINCT
+    CASE
+        WHEN (SELECT MAX(rank_num) FROM NewEmployee) > 1 THEN salary
+        ELSE NULL
+    END AS SecondHighestSalary
+FROM NewEmployee
+WHERE rank_num = CASE
+                     WHEN (SELECT MAX(rank_num) FROM NewEmployee) > 1 THEN 2
+                     ELSE 1
+                 END;
