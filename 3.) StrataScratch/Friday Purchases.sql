a72aabc8c8b59d149c7a460c61014b0aba7d7ac5
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2024-03-05 15:06:10 -0500

    Update Friday Purchases.sql

--- 3.) StrataScratch/Friday Purchases.sql
+++ 3.) StrataScratch/Friday Purchases.sql
@@ -1,3 +1,7 @@
+/* IBM is working on a new feature to analyze user purchasing behavior for all Fridays in the first quarter of the year.
+   For each Friday separately, calculate the average amount users have spent per order. 
+   The output should contain the week number of that Friday and average amount spent. */
+
 WITH WeekNumbers AS
 (
     SELECT GENERATE_SERIES(1, 13) AS week_number
