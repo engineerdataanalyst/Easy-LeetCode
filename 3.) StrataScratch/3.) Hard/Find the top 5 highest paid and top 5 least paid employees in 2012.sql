d10985083993ababb119e68402ed5aa7ae1066b8
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2024-03-26 08:49:08 -0400

    Create Find the top 5 highest paid and top 5 least paid employees in 2012.sql

---
+++ 3.) StrataScratch/3.) Hard/Find the top 5 highest paid and top 5 least paid employees in 2012.sql
@@ -0,0 +1,3 @@
+/* Find the top 5 highest paid and top 5 least paid employees in 2012.
+   Output the employee name along with the corresponding total pay with benefits.
+   Sort records based on the total payment with benefits in ascending order. */
