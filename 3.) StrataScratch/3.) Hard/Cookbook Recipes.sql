b9ceec47a22b76eb7fff90bef3b6cbf209859ae2
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2024-07-30 09:27:28 -0400

    Create Cookbook Recipes.sql

---
+++ 3.) StrataScratch/3.) Hard/Cookbook Recipes.sql
@@ -0,0 +1,31 @@
+WITH page_numbers AS
+(
+    SELECT
+        page_number,
+        ROW_NUMBER() OVER()-1 AS k
+    FROM
+    (
+        SELECT generate_series(0, MAX(page_number), 2) AS page_number
+        FROM cookbook_titles
+    ) c
+),
+modified_cookbook_titles AS
+(
+    SELECT
+        0 AS page_number,
+        NULL AS title
+    
+    UNION ALL
+    
+    SELECT *
+    FROM cookbook_titles
+    WHERE MOD(page_number, 2) = 0
+)
+SELECT
+    p.page_number AS left_page_number,
+    m.title AS left_title,
+    (SELECT title
+     FROM cookbook_titles c
+     WHERE c.page_number = 2*p.k+1) AS right_title
+FROM page_numbers p
+LEFT JOIN modified_cookbook_titles m ON p.page_number = m.page_number;
